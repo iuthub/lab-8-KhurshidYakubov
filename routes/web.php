47e43be51b7946e35c21f3e32224d13190e51937
KhurshidYakubov <khurshid.yakubov@gmail.com>
2019-04-09 12:57:54 +0300

    pr

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog\index');
 });
+
+Route::get('/post', function () {
+    return view('blog\post');
+});
+
+
+
+
